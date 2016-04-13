64584e502a6eebfa711a37bc396b9f86748e178f
Veit Lehmann <levito82@gmail.com>
2016-04-13 23:18:17 +0200

    really no modules

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 
-import './main.css';
-
 export default class Main extends Component {
   render () {
     return (
--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 
-import './widget.css';
-
 export default class Widget extends Component {
   render () {
     return (
